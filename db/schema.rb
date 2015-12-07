f75187dabe35fbc1225f841da14208f7bfa2b44a
isseikz <issey.macky@gmail.com>
2015-12-08 01:24:52 +0900

    set home controller to make a top page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151206175045) do
+ActiveRecord::Schema.define(version: 20151207154939) do
 
   create_table "applicants", force: :cascade do |t|
     t.integer  "requirement_id"
@@ -135,7 +135,7 @@ ActiveRecord::Schema.define(version: 20151206175045) do
     t.datetime "updated_at",     null: false
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "user_profiles", force: :cascade do |t|
     t.string   "name"
     t.integer  "speciality_id"
     t.integer  "job_id"
