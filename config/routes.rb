78fa4f5c422fa17eaa9d485eb41b30910f7093d4
isseikz <issey.macky@gmail.com>
2015-12-12 00:12:35 +0900

    set idea submitting

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to:"home#index"
-  get 'home/index'
+  resources :home
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
