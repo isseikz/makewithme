f723dba87476c2ceab81ad52c1c60acd6e441460
isseikz <issey.macky@gmail.com>
2015-12-31 00:41:58 +0900

    deleted unnecessary design

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -2,18 +2,22 @@ class IdeasController < ApplicationController
   def index
     ideas = Idea.all
     @new_ideas = ideas.order("created_at DESC")
-    # @fav_ideas =
     @idea = Idea.new
   end
 
   def create
-    @idea = Idea.new(idea_params)
-    if @idea.save
-      flash.now[:notice] = "投稿完了！"
-      redirect_to :action => 'index'
+    if user_signed_in?
+      @idea = Idea.new(idea_params)
+      if @idea.save
+        flash.now[:notice] = "投稿完了！"
+        redirect_to :action => 'index'
+      else
+        flash.now[:error] = "予期せぬ理由で投稿に失敗しました。"
+      end
     else
-      flash.now[:error] = "予期せぬ理由で投稿に失敗しました。"
+      redirect_to new_user_registration_path
     end
+
   end
 
   def destroy
